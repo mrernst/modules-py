4f849616b6e8ef870307430be7a29e8290325abd
Markus Ernst <mernst@fias.uni-frankfurt.de>
2019-02-11 17:45:42 +0100

    Fixed faulty normalization

--- src/modules.py
+++ src/modules.py
@@ -672,7 +672,7 @@ class NormalizationModule(OperationModule):
       ?:                    tensor, same shape as x
     """
     casted_x = tf.cast(x, dtype=self.dtype)
-    rescaled_x = (casted_x / 255) * (self.inp_max - self.inp_min) - self.inp_min
+    rescaled_x = (casted_x / 255) * (self.inp_max - self.inp_min) - tf.abs(self.inp_min)
     return rescaled_x
 
 
